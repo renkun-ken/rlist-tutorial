993867e319301383bc2fbd3d99cbeda069977083
Kun Ren <renkun@outlook.com>
2015-01-24 08:13:03 +0800

    fix typo in publish script

--- publish.bat
+++ publish.bat
@@ -6,4 +6,4 @@ git commit --allow-empty -m "Update build gitbook"
 git checkout -b gh-pages
 git add .
 git commit -am "Update build gitbook"
-git push https://github.com/renkun-ken/pipeR-tutorial.git gh-pages --force
+git push https://github.com/renkun-ken/rlist-tutorial.git gh-pages --force
